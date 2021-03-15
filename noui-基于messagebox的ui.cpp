a6bca7d7044a650801ed87f899ad4847b7e03682
309316777 <63705090+309316777@users.noreply.github.com>
2021-03-15 10:58:25 +0800

    Add files via upload

---
+++ noui-基于messagebox的ui.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "noui.h"
+
+#include "ui_interface.h"
+#include "util.h"
+
+#include <cstdio>
+#include <stdint.h>
+#include <string>
+
+static bool noui_ThreadSafeMessageBox(const std::string& message, const std::string& caption, unsigned int style)
+{
+    bool fSecure = style & CClientUIInterface::SECURE;
+    style &= ~CClientUIInterface::SECURE;
+
+    std::string strCaption;
+    // Check for usage of predefined caption/检查预定义标题的用法
+    switch (style) {
+    case CClientUIInterface::MSG_ERROR:
+        strCaption += _("Error");
+        break;
+    case CClientUIInterface::MSG_WARNING:
+        strCaption += _("Warning");
+        break;
+    case CClientUIInterface::MSG_INFORMATION:
+        strCaption += _("Information");
+        break;
+    default:
+        strCaption += caption; // Use supplied caption (can be empty)/使用提供的标题（可以为空）
+    }
+
+    if (!fSecure)
+        LogPrintf("%s: %s\n", strCaption, message);
+    fprintf(stderr, "%s: %s\n", strCaption.c_str(), message.c_str());
+    return false;
+}
+
+static bool noui_ThreadSafeQuestion(const std::string& /* ignored interactive message */, const std::string& message, const std::string& caption, unsigned int style)
+{
+    return noui_ThreadSafeMessageBox(message, caption, style);
+}
+
+static void noui_InitMessage(const std::string& message)
+{
+    LogPrintf("init message: %s\n", message);
+}
+
+void noui_connect()
+{
+    // Connect bitcoind signal handlers/连接比特币信号处理程序
+    uiInterface.ThreadSafeMessageBox.connect(noui_ThreadSafeMessageBox);
+    uiInterface.ThreadSafeQuestion.connect(noui_ThreadSafeQuestion);
+    uiInterface.InitMessage.connect(noui_InitMessage);
+}
